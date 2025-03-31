17f7e5bd6531e597dc83c189a3ef9907b0d8e27b
Mario Silva <mario8988silva@gmail.com>
2025-03-31 09:50:45 +0100

    Sessão 7 : novos ficheiro; inicio de sessão

---
+++ JavaScript/_exercicios/_iniciais/exercicioDOM-1-inicio/exercicioDOM.js
@@ -0,0 +1,16 @@
+/*
+1 - Acrescentar um novo item no inicio e no final da lista
+2 - aplicar a class "cool" a todos os itens da lista
+3 - Acrescentar uma tag "span" dentro do "h2", com o numero de itens da lista
+*/
+
+
+
+
+
+
+
+
+
+
+
